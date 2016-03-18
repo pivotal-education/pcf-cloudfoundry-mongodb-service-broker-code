e990ffbf9887f6c1c0d8029a17ca8246c4d8f79a
d4v3r <ubiquitous_alias@yahoo.com>
2016-03-17 20:56:55 -0500

    changed database name

--- src/main/java/org/springframework/cloud/servicebroker/mongodb/config/MongoConfig.java
+++ src/main/java/org/springframework/cloud/servicebroker/mongodb/config/MongoConfig.java
@@ -16,10 +16,10 @@ import com.mongodb.ServerAddress;
 @EnableMongoRepositories(basePackages = "org.springframework.cloud.servicebroker.mongodb.repository")
 public class MongoConfig extends AbstractMongoConfiguration {
 
-	  @Override
-	  protected String getDatabaseName() {
-	    return "broker";
-	  }
+	@Override
+	protected String getDatabaseName() {
+		return "mongodb-service-broker";
+	}
 
 	@Value("${mongodb.host:localhost}")
 	private String host;
@@ -36,5 +36,5 @@ public class MongoConfig extends AbstractMongoConfiguration {
 	public Mongo mongo() throws Exception {
 		return mongoClient();
 	}
-	
+
 }
